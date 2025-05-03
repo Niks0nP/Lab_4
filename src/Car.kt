159e24dbddcbf721e76ba4402f403194f67b9470
Niks0nP <nikita.niko40@gmail.com>
2025-05-04 01:23:13 +0300

    feat(#2): add class Car

---
+++ src/Car.kt
@@ -0,0 +1,12 @@
+class Car {
+    var speed: Double = 0.0
+    var power: Double = 0.0
+
+    fun setSpeed(newSpeed: Double) {
+        speed = newSpeed
+    }
+
+    fun getSpeed(): Double {
+        return speed
+    }
+}
