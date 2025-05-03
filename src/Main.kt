ff7b5b37ad4cc332e01912b624fd6ddfd644accb
Niks0nP <nikita.niko40@gmail.com>
2025-05-04 00:28:23 +0300

    feat(#1): add function normalizePhoneNumbers

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,40 @@
 import java.io.File
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    readFile()
+    val inputText = readFile()
+
+    val normalizePhoneNumber = normalizePhoneNumbers(text = inputText, newCountryCode = "+1")
+
+    File("task.txt").writeText(normalizePhoneNumber)
+
+    val formattedText = readFile()
+    println(formattedText)
 }
 
-fun readFile() {
+fun readFile(): String {
     val file = File("task.txt")
     val text = file.readText()
 
-    println(text)
+    return text
+}
+
+fun normalizePhoneNumbers(text: String, newCountryCode: String): String {
+    val phoneRegex = Regex("""(?:\+?\d[\d\s\-\(\)]{9,}\d)""")
+
+    return phoneRegex.replace(text) { matchResult ->
+        val raw = matchResult.value
+
+        val digits = raw.filter { it.isDigit() }
+
+        if (digits.length < 10) return@replace raw
+
+        val localNumber = digits.takeLast(10)
+
+        val formatted = "$newCountryCode (${localNumber.substring(0,3)}) " +
+                "${localNumber.substring(3,6)}-" +
+                "${localNumber.substring(6,8)}-" +
+                localNumber.substring(8,10)
+
+        formatted
+    }
 }
